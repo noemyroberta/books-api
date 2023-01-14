fd666099a146c39001fb1307b94d578b428f7d16
Noemy Roberta <noemy.oliveira2@outlook.com>
2023-01-14 01:23:02 -0300

    feature(app): creating route for `get_book_by_id`

--- app.py
+++ app.py
@@ -9,4 +9,11 @@ def get_books():
     return jsonify(books)
 
 
+@app.route('/books/<int:id>', methods=['GET'])
+def get_book_by_id(book_id: int):
+    for book in books:
+        if book.get('id') == book_id:
+            return jsonify(book)
+
+
 app.run(port=5000, host='localhost', debug=True)
